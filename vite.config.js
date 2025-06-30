20b2dc97772ce78b0a6d83833daf6eb771ed24a2
Firdaus Zakari <jannat0firdaus@gmail.com>
2025-06-30 14:16:30 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/health-and-wellness-app",
-  /* server: {
-    port: 3000,
-    open: true,
-  }, */
+  base: "/health-and-wellness-app/",
 })
